b7be0343b31fb1a79c8e244b4b9aeb6e8b4e8def
IDPogolovkin <96434412+IDPogolovkin@users.noreply.github.com>
2023-02-20 17:28:09 +0600

    add sidebar and register with some modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import './App.scss';
+import 'boxicons/css/boxicons.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppLayout from './components/AppLayout';
+import Blank from './pages/Blank';
+import Registration from './components/Registration';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<AppLayout />}>
+                    <Route index element={<Blank />} />
+                    <Route path='/main' element={<Blank />} />
+                    <Route path='/reg' element={<Registration />} />
+                    <Route path='/user' element={<Blank />} />
+                    <Route path='/order' element={<Blank />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
   )
 }
 

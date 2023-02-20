cd3d8075bbfbcf0e04cecad3a0570c22f3671934
PolozovMichael <polozovmisha.sm.nis.edu.kz@gmail.com>
2023-02-21 01:33:51 +0600

    feat: main page scrollable cards section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import Sidebar from './components/Sidebar/Sidebar'
-
 import './styles/global.scss'
+import Sidebar from './components/Sidebar/Sidebar'
+import {Route, Routes} from 'react-router-dom'
+import Mainpage from './components/Mainpage/Mainpage'
 
 const App = () => {
   return (
     <div className="wrapper">
       <Sidebar/>
+      <Routes>
+        <Route path='/' element={<Mainpage/>}></Route>
+        <Route path='/learn' element={<><h1>Learn page</h1></>}></Route>
+        <Route path='/teach' element={<><h1>Teach page</h1></>}></Route>
+        <Route path='/register' element={<><h1>Register page</h1></>}></Route>
+        <Route path='/profile' element={<><h1>Profile page</h1></>}></Route>
+      </Routes>
     </div>
   )
 }

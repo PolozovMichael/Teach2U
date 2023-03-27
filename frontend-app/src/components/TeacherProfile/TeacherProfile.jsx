752cb03131b72dba32b46a57eb6a8783385c8821
PolozovMichael <polozovmisha.sm.nis.edu.kz@gmail.com>
2023-03-27 13:42:43 +0600

    fix: fixed teacher's profile and register pages

--- frontend-app/src/components/TeacherProfile/TeacherProfile.jsx
+++ frontend-app/src/components/TeacherProfile/TeacherProfile.jsx
@@ -3,15 +3,19 @@ import ContactInfoTeach from './ContactInfo/ContactInfoTeach'
 import CoursesTeach from './Courses/CoursesTeach'
 import PersonalInfoTeach from './PersonalInfo Teacher/PersonalInfoTeach'
 import './teacherProfile.css'
+import Sidebar from '../Sidebar/Sidebar'
 
 
 const TeacherProfile = () => {
   return (
-    <div className='settings-block_t'>
+    <div className="main">
+      <Sidebar/>
+      <div className='settings-block_t'>
         <PersonalInfoTeach/>
         <ContactInfoTeach/>
         <h1 className="profile-title_t">Актуальные курсы</h1>
         <CoursesTeach />
+    </div>
     </div>
   )
 }

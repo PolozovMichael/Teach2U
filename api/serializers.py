54d657876e784c3704f896d926a529eca992a925
Timur <72609967+TimurZxc@users.noreply.github.com>
2023-05-20 23:42:19 +0600

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -14,7 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'birth_date', 'surname']
+        fields = ['first_name', 'last_name', 'email', 'birth_date', 'surname', 'telegram']
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
